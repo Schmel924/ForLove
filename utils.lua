8a01cd367f1bdd2668ff759c8f3d7de7a6e48da8
Schmel924 <Schmel924@gmail.com>
2020-08-11 13:01:49 +0300

    Random with real numbers

--- utils.lua
+++ utils.lua
@@ -6,3 +6,8 @@ function UUID()
     end
     return (("xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"):gsub("[xy]", fn))
 end
+
+ 
+function random(min, max)
+    return love.math.random()*(max - min) + min
+end
